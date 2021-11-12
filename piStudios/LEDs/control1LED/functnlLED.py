99ba162fcea1a6083b4b7e89edef1e45b26574ca
okomojacob <jaylabs2030@gmail.com>
2021-11-12 23:21:48 +0300

    Working LED code, :stethoscope:

--- piStudios/LEDs/control1LED/functnlLED.py
+++ piStudios/LEDs/control1LED/functnlLED.py
@@ -1,3 +1,5 @@
+# Working code
+
 import RPi.GPIO as GPIO
 import time #to delay your LED
 
@@ -5,18 +7,17 @@ import time #to delay your LED
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
-ledPin = 12 #Use pin 12(GPIO18)
+ledPin = 7 #Use pin 12(GPIO18)
 GPIO.setup(ledPin, GPIO.OUT)
 
 """"""
 #Flashing
 # while True: #Run forever
 def flash():
-    for i in range(3):
-        print("Turning on")
+    while True:
         GPIO.output(ledPin, GPIO.HIGH)
-        time.sleep(0.5)
-        print("Going Off")
+        time.sleep(0.25)
+          #  print("Going Off")
         GPIO.output(ledPin, GPIO.LOW)
-        time.sleep(0.5)
+        time.sleep(0.25)
 flash()
